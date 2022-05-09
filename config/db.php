578fa1392ec0fc86b04b39fc602abfcff6ec6988
mlp-dw <mlp.dwwb@gmail.com>
2022-05-09 10:51:46 +0200

    --deploy

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-    $db = new PDO("mysql:host=141.94.22.233;dbname=mlpdwwb_comparo_simple;charset=utf8", 'root', 'mlpdwwb');
+    $db = new PDO("mysql:host=141.94.22.233;dbname=mlpdwwb_comparo_simple;charset=utf8", 'mlpdwwb', 'mlpdwwb');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (Exception $e)
